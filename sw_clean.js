d633db804b0e384d037ebd1bfe3779f2211c2993
i2f0726 <i2f0726@gmail.com>
2026-06-05 15:45:33 +0900

    auto deploy 2026-06-05 15:45

--- sw_clean.js
+++ sw_clean.js
@@ -6,7 +6,7 @@
 
 // バージョン番号: index.htmlを更新したらこの数字を上げる
 // （これだけで全ユーザーが自動的に最新版になります）
-const CACHE_VERSION = '2026-06-05-1545';
+const CACHE_VERSION = '2026-06-05-1427';
 const CACHE_NAME = `antelopes-${CACHE_VERSION}`;
 
 // オフライン時にも見られるようにキャッシュするファイル
